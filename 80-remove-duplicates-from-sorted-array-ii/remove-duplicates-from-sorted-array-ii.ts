cbdfb6001e1248b2118016f871b2d19df7711612
Yusupov Radmir <52835356+radmiryusu@users.noreply.github.com>
2025-03-06 08:16:39 +0500

    Time: 70 ms (45.55%) | Memory: 59.8 MB (11.32%) - LeetSync

--- 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.ts
+++ 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.ts
@@ -1,7 +1,11 @@
 function removeDuplicates(nums: number[]): number {
+    if (nums.length <= 2) {
+        return nums.length;
+    }
+
     let index = 1;
     let isCopy = false;
-    
+
     for (let i = 1; i < nums.length; i += 1) {
         if (nums[i - 1] === nums[i]) {
             if (isCopy) continue;
