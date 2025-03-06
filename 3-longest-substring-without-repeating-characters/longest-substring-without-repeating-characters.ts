4fc369866f758c25860305f8374276698e19eba3
Yusupov Radmir <52835356+radmiryusu@users.noreply.github.com>
2025-03-06 09:43:20 +0500

    Time: 5 ms (90.10%) | Memory: 58.9 MB (27.67%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.ts
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.ts
@@ -1,9 +1,29 @@
 function lengthOfLongestSubstring(s: string): number {
+  let mapList = new Map<string, number>();
+  let maxLength = 0;
+  let left = 0;
+
+  for (let right = 0; right < s.length; right++) {
+    const char = s[right];
+
+    if (mapList.has(char) && mapList.get(char)! >= left) {
+      left = mapList.get(char)! + 1;
+    }
+
+    mapList.set(char, right);
+    maxLength = Math.max(maxLength, right - left + 1);
+  }
+
+  return maxLength;
+}
+/*
+// second variant
+function lengthOfLongestSubstring(s: string): number {
     const st = new Set([])
 
     for (let i = 0; i < s.length; i += 1) {
         const symbol = s[i]
-        
+
         if (st.has(symbol)) {
             return Math.max(st.size, lengthOfLongestSubstring(s.slice(1)))
         }
@@ -11,4 +31,4 @@ function lengthOfLongestSubstring(s: string): number {
 
     }
     return st.size;
-};
+};*/
