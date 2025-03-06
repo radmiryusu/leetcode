e22e0fff7a57268e4543e6daaf4a64ec45d19a71
Yusupov Radmir <52835356+radmiryusu@users.noreply.github.com>
2025-03-06 08:16:10 +0500

    Time: 60 ms (83.12%) | Memory: 59.9 MB (9.92%) - LeetSync

---
+++ 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.ts
@@ -0,0 +1,20 @@
+function removeDuplicates(nums: number[]): number {
+    let index = 1;
+    let isCopy = false;
+    
+    for (let i = 1; i < nums.length; i += 1) {
+        if (nums[i - 1] === nums[i]) {
+            if (isCopy) continue;
+
+            nums[index] = nums[i]
+            isCopy = true;
+            index += 1;
+            continue;
+        }
+
+        isCopy = false;
+        nums[index] = nums[i]
+        index += 1;
+    }
+    return index
+};
