9a323fcd88876286d44b32266d90ef387a7737b2
Yusupov Radmir <52835356+radmiryusu@users.noreply.github.com>
2025-03-06 08:57:17 +0500

    Time: 0 ms (100.00%) | Memory: 58.6 MB (18.66%) - LeetSync

---
+++ 169-majority-element/majority-element.ts
@@ -0,0 +1,19 @@
+function majorityElement(nums: number[]): number {
+    let counter = 1;
+    let candidate = nums[0];
+
+    for (let i = 1; i <= nums.length - 1; i++) {
+        const number = nums[i];
+
+        if (candidate === number) {
+            counter++;
+            continue;
+        }
+
+        if (counter === 1) candidate = number
+        else counter--;
+
+    }
+
+    return candidate
+};
