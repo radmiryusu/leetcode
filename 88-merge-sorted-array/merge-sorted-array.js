c4d894e83c73060b3444207f44e733651bcb60cc
Yusupov Radmir <52835356+radmiryusu@users.noreply.github.com>
2025-03-06 08:12:01 +0500

    Time: 0 ms (100.00%) | Memory: 55.5 MB (11.98%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.js
+++ 88-merge-sorted-array/merge-sorted-array.js
@@ -6,8 +6,18 @@
  * @return {void} Do not return anything, modify nums1 in-place instead.
  */
 var merge = function(nums1, m, nums2, n) {
-        const list = [...nums1.slice(0, m), ...nums2.slice(0,n)].sort((a,b)=> a - b)
-    for (let i = 0; i < list.length; i += 1) {
-        nums1[i] = list[i]
+    let mIndex = m - 1;
+    let nIndex = n - 1;
+    let rightIndex = m + n - 1;
+
+    while(nIndex >= 0){
+        if(nums1[mIndex] > nums2[nIndex]) {
+            nums1[rightIndex] = nums1[mIndex];
+            mIndex--;
+        }else {
+            nums1[rightIndex] = nums2[nIndex];
+            nIndex--;
+        }
+        rightIndex--;
     }
 };
